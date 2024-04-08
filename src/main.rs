61cb6a607a33e613e474526f0b98223075bc89e7
MaxNomad <maxnomad123@gmail.com>
2024-04-08 14:50:12 +0300

    fix build

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{fs::{self, OpenOptions}, io::{Seek, SeekFrom}, path::Path, thread, time::Duration};
+use std::{fs::{self, OpenOptions}, io::{Seek, SeekFrom}, thread, time::Duration};
 use serde_json::Value;
 use std::sync::{Arc, Mutex};
 use std::process::Command;
@@ -9,56 +9,77 @@ const DEFAULT_SCALE: f32 = 1.0;
 const DEFAULT_STATE: f32 = 4.0;
 const DEFAULT_COEF: f32 = 0.625;
 
-fn read_float_from_file(file: &mut fs::File, offset: u64) -> f32 {
-    file.seek(SeekFrom::Start(offset)).unwrap();
-    file.read_f32::<BigEndian>().unwrap()
+fn read_float_from_file(file: &mut fs::File, offset: u64) -> Result<f32, std::io::Error> {
+    file.seek(SeekFrom::Start(offset))?;
+    file.read_f32::<BigEndian>()
 }
 
-fn write_float_to_file(file: &mut fs::File, offset: u64, value: f32) {
-    file.seek(SeekFrom::Start(offset)).unwrap();
-    file.write_f32::<BigEndian>(value).unwrap();
+fn write_float_to_file(file: &mut fs::File, offset: u64, value: f32) -> Result<(), std::io::Error> {
+    file.seek(SeekFrom::Start(offset))?;
+    file.write_f32::<BigEndian>(value)
 }
 
-fn initialize_file() {
-    let mut file = OpenOptions::new().read(true).write(true).create(true).open(COUNTER_FILE).unwrap();
-    let metadata = fs::metadata(COUNTER_FILE).unwrap();
+fn initialize_file() -> Result<(), std::io::Error> {
+    let mut file = OpenOptions::new().read(true).write(true).create(true).open(COUNTER_FILE)?;
+    let metadata = fs::metadata(COUNTER_FILE)?;
     let size = metadata.len();
 
     if size < 4 {
-        write_float_to_file(&mut file, 0, 0.0);
+        write_float_to_file(&mut file, 0, 0.0)?;
     }
     if size < 8 {
-        write_float_to_file(&mut file, 4, DEFAULT_SCALE);
+        write_float_to_file(&mut file, 4, DEFAULT_SCALE)?;
     }
     if size < 12 {
-        write_float_to_file(&mut file, 8, 0.0);
+        write_float_to_file(&mut file, 8, 0.0)?;
     }
     if size < 16 {
-        write_float_to_file(&mut file, 12, DEFAULT_STATE);
+        write_float_to_file(&mut file, 12, DEFAULT_STATE)?;
     }
     if size < 20 {
-        write_float_to_file(&mut file, 16, DEFAULT_COEF);
+        write_float_to_file(&mut file, 16, DEFAULT_COEF)?;
     }
+    Ok(())
 }
 
 fn monitor_dwi0(scale_dwi: Arc<Mutex<f32>>) {
     let mut previous_value_dwi: Option<String> = None;
     loop {
-        let output = Command::new("ubus")
+        let output = match Command::new("ubus")
             .args(&["call", "ioman.dwi.dwi0", "status"])
-            .output()
-            .expect("Failed to execute command");
+            .output() {
+                Ok(output) => output,
+                Err(e) => {
+                    eprintln!("Failed to execute command: {:?}", e);
+                    thread::sleep(Duration::from_millis(100));
+                    continue;
+                }
+            };
 
         let result_str = String::from_utf8_lossy(&output.stdout);
         let data: Value = serde_json::from_str(&result_str).unwrap_or_else(|_| serde_json::json!({}));
         let current_value_dwi = data["value"].as_str().unwrap_or("");
 
         if current_value_dwi == "1" && previous_value_dwi.as_deref() != Some("1") {
-            let mut file = OpenOptions::new().read(true).write(true).open(COUNTER_FILE).unwrap();
+            let mut file = match OpenOptions::new().read(true).write(true).open(COUNTER_FILE) {
+                Ok(file) => file,
+                Err(e) => {
+                    eprintln!("Failed to open file: {:?}", e);
+                    thread::sleep(Duration::from_millis(100));
+                    continue;
+                }
+            };
             let scale = scale_dwi.lock().unwrap();
-            let counter_dwi = read_float_from_file(&mut file, 0) + *scale;
-            write_float_to_file(&mut file, 0, counter_dwi);
-            println!("dwi0 - New data: {} with scale {} at {:?}", counter_dwi, *scale, chrono::Local::now());
+            if let Ok(counter_dwi) = read_float_from_file(&mut file, 0) {
+                let new_counter_dwi = counter_dwi + *scale;
+                if let Err(e) = write_float_to_file(&mut file, 0, new_counter_dwi) {
+                    eprintln!("Failed to write to file: {:?}", e);
+                } else {
+                    println!("dwi0 - New data: {} with scale {} at {:?}", new_counter_dwi, *scale, chrono::Local::now());
+                }
+            } else {
+                eprintln!("Failed to read from file.");
+            }
         }
 
         previous_value_dwi = Some(current_value_dwi.to_string());
@@ -68,39 +89,51 @@ fn monitor_dwi0(scale_dwi: Arc<Mutex<f32>>) {
 
 fn monitor_acl0(state_acl0: Arc<Mutex<f32>>, coef_acl0: Arc<Mutex<f32>>) {
     loop {
-        let output = Command::new("ubus")
+        let output = match Command::new("ubus")
             .args(&["call", "ioman.acl.acl0", "status"])
-            .output()
-            .expect("Failed to execute command");
+            .output() {
+                Ok(output) => output,
+                Err(e) => {
+                    eprintln!("Failed to execute command: {:?}", e);
+                    thread::sleep(Duration::from_secs(1));
+                    continue;
+                }
+            };
 
         let result_str = String::from_utf8_lossy(&output.stdout);
         let data: Value = serde_json::from_str(&result_str).unwrap_or_else(|_| serde_json::json!({}));
         let current_value_acl: f32 = data["value"].as_str().unwrap_or("0").parse().unwrap_or(0.0);
 
-        let mut file = OpenOptions::new().read(true).write(true).open(COUNTER_FILE).unwrap();
+        let mut file = match OpenOptions::new().read(true).write(true).open(COUNTER_FILE) {
+            Ok(file) => file,
+            Err(e) => {
+                eprintln!("Failed to open file: {:?}", e);
+                thread::sleep(Duration::from_secs(1));
+                continue;
+            }
+        };
         let state = *state_acl0.lock().unwrap();
         let coef = *coef_acl0.lock().unwrap();
         let adjusted_value = ((current_value_acl - state) * coef).max(0.0);
-        write_float_to_file(&mut file, 8, adjusted_value);
-        println!("acl0 - New pressure value: {} with coef {} and state {} at {:?}", adjusted_value, coef, state, chrono::Local::now());
-        
+        if let Err(e) = write_float_to_file(&mut file, 8, adjusted_value) {
+            eprintln!("Failed to write to file: {:?}", e);
+        } else {
+            println!("acl0 - New pressure value: {} with coef {} and state {} at {:?}", adjusted_value, coef, state, chrono::Local::now());
+        }
+
         thread::sleep(Duration::from_secs(1));
     }
 }
 
 fn main() {
-    if !Path::new(COUNTER_FILE).exists() {
-        if let Some(p) = Path::new(COUNTER_FILE).parent() {
-            fs::create_dir_all(p).unwrap();
-        }
-        initialize_file();
-    } else {
-        initialize_file();
+    if let Err(e) = initialize_file() {
+        eprintln!("Failed to initialize file: {:?}", e);
+        return;
     }
 
-    let scale_dwi = Arc::new(Mutex::new(read_float_from_file(&mut fs::File::open(COUNTER_FILE).unwrap(), 4)));
-    let state_acl0 = Arc::new(Mutex::new(read_float_from_file(&mut fs::File::open(COUNTER_FILE).unwrap(), 12)));
-    let coef_acl0 = Arc::new(Mutex::new(read_float_from_file(&mut fs::File::open(COUNTER_FILE).unwrap(), 16)));
+    let scale_dwi = Arc::new(Mutex::new(read_float_from_file(&mut fs::File::open(COUNTER_FILE).unwrap(), 4).unwrap()));
+    let state_acl0 = Arc::new(Mutex::new(read_float_from_file(&mut fs::File::open(COUNTER_FILE).unwrap(), 12).unwrap()));
+    let coef_acl0 = Arc::new(Mutex::new(read_float_from_file(&mut fs::File::open(COUNTER_FILE).unwrap(), 16).unwrap()));
 
     let scale_dwi_clone = Arc::clone(&scale_dwi);
     let state_acl0_clone = Arc::clone(&state_acl0);
